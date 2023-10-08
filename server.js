4e39853f4d5e9ff54f40ffbd2c6fa84849276b6b
SergeyMushtukov <hrystik90@gmail.com>
2023-10-08 23:50:24 +0300

    Подчистил пароли

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ import app from "./app.js";
 
 const {DB_HOST, PORT=3000} = process.env;
 
-// 6mqXHh7namqnu
-
-// const DB_HOST =
-//   "mongodb+srv://Sergey:6mqXHh7namqnu@cluster0.x8du0ty.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=AtlasApp";
 
 mongoose
   .connect(DB_HOST)
