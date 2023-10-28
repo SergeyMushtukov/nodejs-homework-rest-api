641a03005e6b1d0ab2060d777a72ffff2b334e0d
SergeyMushtukov <hrystik90@gmail.com>
2023-10-28 22:15:57 +0300

    Закончил ДЗ-6

--- models/User.js
+++ models/User.js
@@ -23,6 +23,13 @@ const userSchema = new Schema({
     type: String,
     default: null,
   },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+  },
 });
 
 userSchema.post("save", handleSaveError);
@@ -41,6 +48,12 @@ export const userSignupSchema = Joi.object({
 export const userSigninSchema = Joi.object({
   password: Joi.string().min(6).required(),
   email: Joi.string().email().required(),
+});
+
+export const userEmailSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    "any.required": `missing required field "email"`,
+  }),
 });
 
 const User = model("user", userSchema);
